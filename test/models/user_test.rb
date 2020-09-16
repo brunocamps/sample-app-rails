0e4ce8b7e826da001f6ba845a77ea25e90baf09e
brunocamps <bruno.mcamps@outlook.com>
2020-09-16 10:13:37 -0300

    Updated user model and user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,6 +55,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@Example.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
 
 
