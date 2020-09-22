bb0a8805730ea01cc51c0b1501c2046d1fa82f19
Bruno Campos <bruno.mcamps@outlook.com>
2020-09-22 13:54:48 -0300

    Completed signup process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       #@user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      flash[:success] = "Welcome to the sample app!."
+      redirect_to @user
     else
       render 'new'
     end
