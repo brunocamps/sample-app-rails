90e446eff960f79c610357485e5f80374ac6fcea
Bruno Campos <bruno.mcamps@outlook.com>
2020-09-29 13:41:55 -0300

    Login custom header working

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
     #logs in the given user
     def log_in(user)
-        session[:user_id] = user_id
+        session[:user_id] = user.id
     end
 
     # Returns the current logged-in u+ser (if any)
