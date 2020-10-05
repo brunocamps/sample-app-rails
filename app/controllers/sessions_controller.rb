6aeca4c461a163d1c45fba5e79a3d78e923afcdf
Bruno Campos <bruno.mcamps@outlook.com>
2020-10-04 21:25:15 -0300

    Did not implement beyond 9.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) :forget(user)
+      #remember user
       redirect_to user
     else
       #Create an error message
@@ -17,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in? #conditional
     redirect_to root_url
   end
 end
