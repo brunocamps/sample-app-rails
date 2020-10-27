eac4af7e9145cbd876d17c7598c50841b9d8ca6b
Bruno Campos <bruno.mcamps@outlook.com>
2020-10-26 22:47:55 -0300

    Created activation token callback

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy' 
   resources :users
+  resources :account_activations, only: [:edit]
 
   #get signup_path is possible because I've declared signup in the routes.rb file
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
